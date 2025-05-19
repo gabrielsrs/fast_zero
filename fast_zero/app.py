8a0b2daf42333a0a723d5485899a7bbfd9cce75d
garshi <garshi@git>
2025-05-18 23:51:40 -0300

    a2: introducao ao desenvolvimento web

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+from fast_zero.schemas import Message
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', response_model=Message)
 def read_root():
     return {'message': 'Hii'}
+
+
+@app.get('/a2', response_class=HTMLResponse)
+def a2():
+    return '<h1>ola mundo</h1>'
