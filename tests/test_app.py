5d73d5b83d07ca989a719fe1f9055fc24f361f52
garshi <55472396+gabrielsrs@users.noreply.github.com>
2025-06-06 09:12:01 -0300

    a5: integrando banco de dados a api

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,7 @@
 from http import HTTPStatus
 
+from fast_zero.schemas import UserPublic
+
 
 def test_root_return_hii(client):
     response = client.get('/')
@@ -33,22 +35,22 @@ def test_create_user(client):
     }
 
 
-def test_read_users(client):
+def test_read_users_without_user(client):
     response = client.get('/users/')
 
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {
-        'users': [
-            {
-                'id': 1,
-                'username': 'alice',
-                'email': 'alice@example.com',
-            },
-        ]
-    }
+    assert response.json() == {'users': []}
 
 
-def test_update_user(client):
+def test_read_users(client, user):
+    user_schema = UserPublic.model_validate(user).model_dump()
+    response = client.get('/users/')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'users': [user_schema]}
+
+
+def test_update_user(client, user):
     response = client.put(
         '/users/1',
         json={
@@ -98,15 +100,11 @@ def test_read_not_found_user(client):
     assert response.json() == {'detail': 'user_id=2 not found'}
 
 
-def test_delete_user(client):
+def test_delete_user(client, user):
     response = client.delete('/users/1')
 
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {
-        'id': 1,
-        'username': 'bob',
-        'email': 'bob@example.com',
-    }
+    assert response.json() == {'message': 'User deleted'}
 
 
 def test_delete_not_found_user(client):
@@ -114,3 +112,26 @@ def test_delete_not_found_user(client):
 
     assert response.status_code == HTTPStatus.NOT_FOUND
     assert response.json() == {'detail': 'user_id=2 not found'}
+
+
+def test_update_integrity_error(client, user):
+    client.post(
+        '/users',
+        json={
+            'username': 'Jhon Doe',
+            'email': 'jhon@email.com',
+            'password': 'jhon@123',
+        },
+    )
+
+    response = client.put(
+        f'users/{user.id}',
+        json={
+            'username': 'Jhon Doe',
+            'email': 'jhon@email.com',
+            'password': 'jhon@123',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.CONFLICT
+    assert response.json() == {'detail': 'Username or Email already exist'}
