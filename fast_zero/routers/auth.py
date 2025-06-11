700b1eaa38fa9dac09ee22f8f6a8ade7f8bbf4ea
garshi <55472396+gabrielsrs@users.noreply.github.com>
2025-06-10 23:25:19 -0300

    a9: tornando o sistema de autenticacao robusto

--- fast_zero/routers/auth.py
+++ fast_zero/routers/auth.py
@@ -11,6 +11,7 @@ from fast_zero.models import User
 from fast_zero.schemas import Token
 from fast_zero.security import (
     create_access_token,
+    get_current_user,
     verify_password,
 )
 
@@ -30,15 +31,24 @@ async def login_for_access_token(
 
     if not user:
         raise HTTPException(
-            datail='Incorrect email or password',
+            detail='Incorrect email or password',
             status_code=HTTPStatus.UNAUTHORIZED,
         )
 
     if not verify_password(form_data.password, user.password):
         raise HTTPException(
-            datail='Incorrect email or password',
+            detail='Incorrect email or password',
             status_code=HTTPStatus.UNAUTHORIZED,
         )
 
     access_token = create_access_token({'sub': user.email})
     return {'access_token': access_token, 'token_type': 'Bearer'}
+
+
+@router.post('/refresh_token', response_model=Token)
+async def refresh_access_token(
+    user: Annotated[User, Depends(get_current_user)],
+):
+    new_access_token = create_access_token(data={'sub': user.email})
+
+    return {'access_token': new_access_token, 'token_type': 'Bearer'}
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 from contextlib import contextmanager
 from datetime import datetime
 
+import factory
 import pytest
 import pytest_asyncio
 from fastapi.testclient import TestClient
@@ -68,12 +69,22 @@ def mock_db_time():
 
 @pytest_asyncio.fixture
 async def user(session: AsyncSession):
-    password = 'alice@123'
-    user = User(
-        username='alice',
-        email='alice@example.com',
-        password=get_password_hash(password),
-    )
+    password = 'testtest'
+    user = UserFactory(password=get_password_hash(password))
+
+    session.add(user)
+    await session.commit()
+    await session.refresh(user)
+
+    user.clean_password = password
+
+    return user
+
+
+@pytest_asyncio.fixture
+async def other_user(session: AsyncSession):
+    password = 'testtest'
+    user = UserFactory(password=get_password_hash(password))
 
     session.add(user)
     await session.commit()
@@ -97,3 +108,12 @@ def token(client, user):
 @pytest.fixture
 def settings():
     return Settings()
+
+
+class UserFactory(factory.Factory):
+    class Meta:
+        model = User
+
+    username = factory.Sequence(lambda n: f'test{n}')
+    email = factory.LazyAttribute(lambda obj: f'{obj.username}@test.com')
+    password = factory.LazyAttribute(lambda obj: f'{obj.username}@example.com')
